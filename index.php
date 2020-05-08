d2455426a1c4682689fa78e9dc2719e15831871c
Opro Darius <opropro@gmail.com>
2020-05-08 16:34:18 +0300

    Basic HTML Template Creation

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>RankingCoach - Web Recruitment Exercise</title>
+        <meta name="description" content="RankingCoach - Web Recruitment Exercise">
+        <meta name="author" content="Opro Darius">
+        <link rel="stylesheet" href="assets/css/style.css?v=1.0">
+    </head>
+
+    <body>
+        <script src="assets/js/scripts.js" async></script>
+    </body>
+</html>
